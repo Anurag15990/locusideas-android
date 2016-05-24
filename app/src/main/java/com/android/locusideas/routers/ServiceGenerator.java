480aa011485e5a2eacf80b20a126481e3030ab76
Satya Iyengar <Isatya1992@gmail.com>
2016-05-24 21:20:55 +0530

    - Rename class - Add generic method to create different service instances

---
+++ app/src/main/java/com/android/locusideas/routers/ServiceGenerator.java
@@ -0,0 +1,44 @@
+package com.android.locusideas.routers;
+
+import com.locusideas.locusideas.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by anurag on 5/1/16.
+ */
+public class ServiceGenerator {
+    private static Retrofit sInstance;
+
+    public static Retrofit getRetrofitInstance(){
+        if (sInstance == null){
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+            if(BuildConfig.DEBUG) {
+                logging.setLevel(Level.BODY);
+            } else {
+                logging.setLevel(Level.NONE);
+            }
+
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(logging);
+
+            sInstance = new Retrofit
+                    .Builder()
+                    .baseUrl(BuildConfig.API_URL)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return sInstance;
+    }
+
+    public static <S> S createService(Class <S> apiService, Retrofit retrofit){
+        return retrofit.create(apiService);
+    }
+
+}
