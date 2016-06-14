bddedc093543771ba47f5f1d5b51ffaa02e670cf
Satya Iyengar <Isatya1992@gmail.com>
2016-06-14 19:28:26 +0530

    Extend base module class

--- app/src/main/java/com/android/locusideas/auth/SignIn/injection/SignInModule.java
+++ app/src/main/java/com/android/locusideas/auth/SignIn/injection/SignInModule.java
@@ -2,7 +2,9 @@ package com.android.locusideas.auth.SignIn.injection;
 
 import com.android.locusideas.auth.SignIn.SignInContract;
 import com.android.locusideas.auth.SignIn.SignInPresenter;
+import com.android.locusideas.core.BaseModule;
 import com.android.locusideas.core.data.auth.AuthDataContract;
+import com.android.locusideas.core.ui.BaseView;
 import com.android.locusideas.core.utils.injection.PerActivity;
 
 import dagger.Module;
@@ -12,17 +14,15 @@ import dagger.Provides;
  * Created on 28/05/16.
  */
 @Module
-public class SignInModule {
+public class SignInModule<T extends SignInContract.View> extends BaseModule<T> {
 
-    private SignInContract.View mView;
-
-    public SignInModule(SignInContract.View view){
-        mView = view;
+    public SignInModule(T view){
+        super(view);
     }
 
     @Provides
     @PerActivity
     public SignInContract.Presenter getSignInPresenter(AuthDataContract.Services authServices){
-        return new SignInPresenter(mView, authServices);
+        return new SignInPresenter(getView(), authServices);
     }
 }
