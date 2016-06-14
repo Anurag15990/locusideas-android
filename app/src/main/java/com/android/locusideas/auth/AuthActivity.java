4d7415d67f707591023eaa5ae363a82661311b85
Satya Iyengar <Isatya1992@gmail.com>
2016-06-14 15:20:54 +0530

    Remove unused code

--- app/src/main/java/com/android/locusideas/auth/AuthActivity.java
+++ app/src/main/java/com/android/locusideas/auth/AuthActivity.java
@@ -44,13 +44,6 @@ public class AuthActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     public void openSignInActivity(View view) {
         Intent signInIntent = new Intent(this, SignInActivity.class);
         startActivity(signInIntent);
@@ -59,21 +52,6 @@ public class AuthActivity extends AppCompatActivity {
     public void openSignUpActivity(View view) {
         Intent signUpIntent = new Intent(this, SignUpActivity.class);
         startActivity(signUpIntent);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -87,6 +65,6 @@ public class AuthActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         AppEventsLogger.deactivateApp(this);
-
     }
+
 }
