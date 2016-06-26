98493dbceed3f15033e82c9b9034000ead21ae23
Satya Iyengar <Isatya1992@gmail.com>
2016-06-26 12:17:27 +0530

    Fragments provider for main tabs

--- app/src/main/java/com/android/locusideas/home/MainShellFragmentsProvider.java
+++ app/src/main/java/com/android/locusideas/home/MainShellFragmentsProvider.java
@@ -1,9 +1,13 @@
 package com.android.locusideas.home;
 
+import com.android.locusideas.core.ui.TabIcon;
 import com.android.locusideas.home.design.DesignsFragment;
 import com.android.locusideas.home.designers.DesignersFragment;
 import com.android.locusideas.home.requirements.RequirementsFragment;
 import com.android.locusideas.home.settings.SettingsFragment;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created on 25/06/16.
@@ -11,20 +15,63 @@ import com.android.locusideas.home.settings.SettingsFragment;
 
 public class MainShellFragmentsProvider {
 
-    public DesignsFragment getDesignsFragment(){
+    public BaseHomeFragment provideFragmentAtPosition(int position){
+        BaseHomeFragment item = null;
+
+        if (position == 0){
+            item = provideDesignsFragment();
+        } else if (position == 1){
+            item = provideDesignersFragment();
+        } else if (position == 2){
+            item = provideRequirementsFragment();
+        } else if (position == 3){
+            item = provideSettingsFragment();
+        }
+
+        return item;
+    }
+
+    private DesignsFragment provideDesignsFragment(){
         return DesignsFragment.getInstance();
     }
 
-    public DesignersFragment getDesignersFragment(){
+    private DesignersFragment provideDesignersFragment(){
         return DesignersFragment.getInstance();
     }
 
-    public RequirementsFragment getRequirementsFragment(){
+    private RequirementsFragment provideRequirementsFragment(){
         return RequirementsFragment.getInstance();
     }
 
-    public SettingsFragment getSettingsFragment(){
+    private SettingsFragment provideSettingsFragment(){
         return SettingsFragment.getInstance();
+    }
+
+    //TODO find a way to get icons from respective fragments
+    public List<AHBottomNavigationItem> getFragmentTabIcons(){
+        List<AHBottomNavigationItem> items = new ArrayList<>();
+
+        for(int i = 0; i < getFragmentCount(); i++){
+            TabIcon icon = null;
+
+            if (i == 0){
+                icon = DesignsFragment.getTabIcon();
+            } else if (i == 1){
+                icon = DesignersFragment.getTabIcon();
+            } else if (i == 2){
+                icon = RequirementsFragment.getTabIcon();
+            } else if (i == 3){
+                icon = SettingsFragment.getTabIcon();
+            }
+
+            items.add(new AHBottomNavigationItem(icon.getTitleStringRes(), icon.getIconRes(), icon.getColorRes()));
+        }
+
+        return items;
+    }
+
+    public int getFragmentCount(){
+        return 4;
     }
 
 }
