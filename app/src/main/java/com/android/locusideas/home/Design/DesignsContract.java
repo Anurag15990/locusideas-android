163539e19129fef2cacffd58e2742c9193c07920
Satya Iyengar <Isatya1992@gmail.com>
2016-06-28 19:52:44 +0530

    Contract for designs screen view and presenter

---
+++ app/src/main/java/com/android/locusideas/home/Design/DesignsContract.java
@@ -0,0 +1,17 @@
+package com.android.locusideas.home.design;
+
+import com.android.locusideas.core.ui.BasePresenter;
+import com.android.locusideas.core.ui.BaseView;
+
+/**
+ * Created on 28/06/16.
+ */
+
+public interface DesignsContract {
+    interface View extends BaseView{
+    }
+
+    interface Presenter extends BasePresenter{
+
+    }
+}
