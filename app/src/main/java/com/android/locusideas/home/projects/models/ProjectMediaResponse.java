49b154e647c17b4b6794d5fad501f31beb4d6863
Satya Iyengar <Isatya1992@gmail.com>
2016-07-07 23:18:23 +0530

    Model for project module

---
+++ app/src/main/java/com/android/locusideas/home/projects/models/ProjectMediaResponse.java
@@ -0,0 +1,15 @@
+package com.android.locusideas.home.projects.models;
+
+import java.util.List;
+
+/**
+ * Created on 07/07/16.
+ */
+
+public class ProjectMediaResponse {
+    List<ProjectMedia> projectMedias;
+
+    public List<ProjectMedia> getProjectMedias() {
+        return projectMedias;
+    }
+}
