b127ae50113d6ecea03c2d017f70bfff5cdbdd14
Satya Iyengar <Isatya1992@gmail.com>
2016-07-07 23:19:51 +0530

    Fetch project media from remote

--- app/src/main/java/com/android/locusideas/core/data/services/ProjectsService.java
+++ app/src/main/java/com/android/locusideas/core/data/services/ProjectsService.java
@@ -1,8 +1,10 @@
 package com.android.locusideas.core.data.services;
 
 import com.android.locusideas.core.data.models.response.ProjectsResponse;
+import com.android.locusideas.home.projects.models.ProjectMediaResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created on 13/06/16.
@@ -12,5 +14,8 @@ public interface ProjectsService {
 
     @GET("/api/projects")
     Call<ProjectsResponse> loadProjects();
+
+    @GET("/api/projects/{projectId}/medias")
+    Call<ProjectMediaResponse> loadProjectMedias(@Path("projectId")String projectId);
 
 }
