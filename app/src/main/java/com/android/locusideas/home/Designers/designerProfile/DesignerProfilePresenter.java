f049185f53a5b9a30ca01578f2e11f0f992a2172
Satya Iyengar <Isatya1992@gmail.com>
2016-07-31 20:12:16 +0530

    Create designer profile presenter

---
+++ app/src/main/java/com/android/locusideas/home/Designers/designerProfile/DesignerProfilePresenter.java
@@ -0,0 +1,13 @@
+package com.android.locusideas.home.designers.designerProfile;
+
+import com.android.locusideas.core.utils.mvp.BasePresenter;
+import com.android.locusideas.home.designers.DesignersView;
+
+/**
+ * Created on 24/07/16.
+ */
+
+public class DesignerProfilePresenter extends BasePresenter<DesignersView> {
+
+
+}
